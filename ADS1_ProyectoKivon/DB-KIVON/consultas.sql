e3e727995dea8a8571c81ad59ee252b633cce4a2
Jhazziel <Chocotron4836>
2017-11-02 20:42:38 -0400

    modificacion completa de la base de datos

--- ADS1_ProyectoKivon/DB-KIVON/consultas.sql
+++
@@ -1,5 +0,0 @@
-select idusuario from usuario where ci = "8333482"
-union
-select idcliente from cliente where ci="8333482"
-union
-select idreferenciausuario from referenciausuario where ci="8333482";
